75f805ba47cd379baa8d4790a25fc1c6b6521398
wangjianling <wangjianling@tuandai.com>
2018-05-07 11:49:55 +0800

    add

--- springboot-mq/src/main/java/com/example/springbootmq/SpringbootMqApplication.java
+++ springboot-mq/src/main/java/com/example/springbootmq/SpringbootMqApplication.java
@@ -1,10 +1,17 @@
 package com.example.springbootmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootMqApplication {
+
+	@Bean
+	public Queue helloQueue() {
+		return new Queue("helloQueue");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMqApplication.class, args);
